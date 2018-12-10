dae0cee88dcec6e4c5b9fe57fc76773b874f79aa
shodgearo <shodkvest@gmail.com>
2018-12-10 12:50:16 +0600

    add initial data base and add to git ignore database

--- app.rb
+++ app.rb
@@ -2,6 +2,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+  @db = SQLite3::Database.new 'astroblog'
+  @db.results_as_hash = true
+end
+
+before do
+  init_db
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -14,5 +24,5 @@ end
 post '/new' do
   area = params[:area]
 
-  erb "#{area}"
+  erb "Вы ввели: #{area}"
 end
